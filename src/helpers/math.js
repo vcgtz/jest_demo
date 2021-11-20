4cc4eb4da10a8395424fc77f5d413f2b6bd06208
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-11-20 13:49:42 -0600

    Initial commit

---
+++ src/helpers/math.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const pow = (base, exponent) => base ** exponent;
+
+module.exports = {
+    sum,
+    multiply,
+    pow
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const math = require('./helpers/math');
+
+console.log('Math functions');
+console.log(`Sum: ${math.sum(10, 20)}`);
+console.log(`Multiply: ${math.multiply(5, 5)}`);
+console.log(`Pow: ${math.sum(2, 8)}`);
